76890099948966c69d37742ee12a35bb322f773d
Sean Cross <xobs@kosagi.com>
2014-07-07 11:44:07 +0800

    senoko: Initial project commit

---
+++ senoko/bionic.h
@@ -0,0 +1,4 @@
+#ifndef __BIONIC_H__
+#define __BIONIC_H__
+#   define ULONG_MAX	4294967295UL
+#endif /* __BIONIC_H__ */
---
+++ senoko/senoko.h
@@ -0,0 +1,13 @@
+#ifndef __SENOKO_H__
+#define __SENOKO_H__
+
+extern const char *gitversion;
+
+#define SENOKO_OS_VERSION_MAJOR 2
+#define SENOKO_OS_VERSION_MINOR 0
+
+#define serialDriver (&SD1)
+#define stream ((BaseSequentialStream *)&SD1)
+
+
+#endif /* __SENOKO_H__ */
