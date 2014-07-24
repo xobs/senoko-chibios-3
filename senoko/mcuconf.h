3492921cb256c2b9491148742ff8c5c89eff3c2d
Sean Cross <xobs@kosagi.com>
2014-07-24 10:39:43 +0800

    senoko: Run MCU at 36 MHz off PLL
    Prior to this, SW was set to take the clock from the 8 MHz HSI.  Change
    it to come off of the 36 MHz PLL.

--- senoko/mcuconf.h
+++ senoko/mcuconf.h
@@ -38,7 +38,7 @@
 #define STM32_LSI_ENABLED                   FALSE
 #define STM32_HSE_ENABLED                   FALSE
 #define STM32_LSE_ENABLED                   TRUE
-#define STM32_SW                            STM32_SW_HSI
+#define STM32_SW                            STM32_SW_PLL
 #define STM32_PLLSRC                        STM32_PLLSRC_HSI
 #define STM32_PLLXTPRE                      STM32_PLLXTPRE_DIV1
 #define STM32_PLLMUL_VALUE                  9
