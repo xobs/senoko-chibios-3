f520096fd3ffd6df2fa09dea3b2eb0384eef9269
Sean Cross <xobs@kosagi.com>
2014-07-30 19:01:02 +0800

    senoko: Break shell and watchdog out from main.c
    In an effort to keep main.c from growing too large, use events, and
    break shell and watchdog code into their own threads.

--- senoko/senoko-shell.h
+++ senoko/senoko-shell.h
@@ -1,10 +1,9 @@
 #ifndef __SENOKO_SHELL_H__
 #define __SENOKO_SHELL_H__
 
-/* Returns TRUE if the shell has terminated (or hasn't been launched yet) */
-int shellTerminated(void);
+void senokoShellInit(void);
 
 /* Runs a new shell thread */
-void shellRestart(void);
+void senokoShellRestart(void);
 
 #endif /* __SENOKO_SHELL_H__ */
