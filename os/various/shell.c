c2269baa7ad5fd5c617e1b1ac7ebbd4c4af5dbe1
Sean Cross <xobs@kosagi.com>
2014-07-30 18:00:55 +0800

    chibios: shell: Fix backspace command
    Backspace wasn't working.  It now accepts 0x7f in addition to '\b',
    and prints '\b' to scribble over the line.  How both "Backspace" and ^H work.

--- os/various/shell.c
+++ os/various/shell.c
@@ -282,11 +282,11 @@ bool shellGetLine(BaseSequentialStream *chp, char *line, unsigned size) {
       chprintf(chp, "^D");
       return TRUE;
     }
-    if (c == 8) {
+    if (c == '\b' || c == 0x7f) {
       if (p != line) {
-        chSequentialStreamPut(chp, c);
+        chSequentialStreamPut(chp, '\b');
         chSequentialStreamPut(chp, 0x20);
-        chSequentialStreamPut(chp, c);
+        chSequentialStreamPut(chp, '\b');
         p--;
       }
       continue;
