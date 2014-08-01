939b089231ac6c100728e18c2326dc9d1a3e0cfe
Sean Cross <xobs@kosagi.com>
2014-08-01 18:12:34 +0800

    cmd-threads: Print out thread name

--- phage2014/cmd-threads.c
+++ phage2014/cmd-threads.c
@@ -28,13 +28,14 @@ void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[])
     chprintf(chp, "Usage: threads\r\n");
     return;
   }
-  chprintf(chp, "    addr    stack prio refs     state time\r\n");
+  chprintf(chp, "    addr    stack prio refs   state time  name\r\n");
   tp = chRegFirstThread();
   do {
-    chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s\r\n",
+    chprintf(chp, "%.8lx %.8lx %4lu %4lu %12s  %-10s\r\n",
       (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
       (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
-      states[tp->p_state]);
+      states[tp->p_state],
+      tp->p_name);
     tp = chRegNextThread(tp);
   } while (tp != NULL);
 }
