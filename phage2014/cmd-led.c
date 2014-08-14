2ed18ac4be1e2b6cb0519785a76637328facb0a6
Sean Cross <xobs@kosagi.com>
2014-08-14 16:16:43 +0800

    cmd-led: Fix warnings
    There were two unused variables.  Fix them.

--- phage2014/cmd-led.c
+++ phage2014/cmd-led.c
@@ -7,11 +7,8 @@
 #include "phage.h"
 #include "ledDriver.h"
 
-static uint32_t addr = 0;
 void cmd_led(BaseSequentialStream *chp, int argc, char *argv[])
 {
-  uint8_t dat[11];
-
   (void)argv;
 
   chprintf(chp, "argc: %d\r\n", argc);
