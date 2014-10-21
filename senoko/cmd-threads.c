c2a875f038926943274188f2d3af9af007af5012
Sean Cross <xobs@kosagi.com>
2014-10-21 18:16:34 +0800

    cmd-threads: Print out PC location
    The program counter is really interesting in threads.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- senoko/cmd-threads.c
+++ senoko/cmd-threads.c
@@ -28,11 +28,11 @@ void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[])
     chprintf(chp, "Usage: threads\r\n");
     return;
   }
-  chprintf(chp, "    addr    stack prio refs   state time  name\r\n");
+  chprintf(chp, "    addr       pc    stack prio refs   state time  name\r\n");
   tp = chRegFirstThread();
   do {
-    chprintf(chp, "%.8lx %.8lx %4lu %4lu %12s  %-10s\r\n",
-      (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
+    chprintf(chp, "%.8lx %.8lx %.8lx %4lu %4lu %12s  %-10s\r\n",
+      (uint32_t)tp, (uint32_t)tp->p_ctx.r13->lr, (uint32_t)tp->p_ctx.r13,
       (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
       states[tp->p_state],
       tp->p_name);
