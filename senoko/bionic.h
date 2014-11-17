9372ab590836ce99b4b3f20e757580d2469eac9d
Sean Cross <xobs@kosagi.com>
2014-11-17 12:07:18 +0800

    bionic: Add memset to header file
    We define this function.  Add its signature to the header file to fix a
    warning in gg.c.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- senoko/bionic.h
+++ senoko/bionic.h
@@ -7,6 +7,7 @@
 
 int strcasecmp(const char *s1, const char *s2);
 void *memcpy(void *dst0, const void *src0, size_t length);
+void *memset(void *dst0, int val, size_t length);
 unsigned long strtoul(const char *nptr, char **endptr, int base);
 
 #endif /* __BIONIC_H__ */
