9d737743c65b99698c380b5e2d0a0e7b2969a9c5
Sean Cross <xobs@kosagi.com>
2014-11-17 14:46:07 +0800

    senoko-slave: Update header file with new layout
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- senoko/senoko-slave.h
+++ senoko/senoko-slave.h
@@ -6,12 +6,10 @@ struct i2c_registers {
   uint8_t version_major;    /* 0x01 */
   uint8_t version_minor;    /* 0x02 */
   uint8_t uptime[4];        /* 0x03 - 0x06 */
-  uint8_t power;            /* 0x07 */
-  uint8_t wdt_seconds;      /* 0x08 */
-  uint8_t ier;              /* 0x09 */
-  uint8_t irq_status;       /* 0x0a */
-  uint8_t key_status;       /* 0x0b */
-  uint8_t padding0[4];      /* 0x0c - 0x0f */
+  uint8_t irq_enable;       /* 0x07 */
+  uint8_t irq_status;       /* 0x08 */
+  uint8_t padding0[6];      /* 0x09 - 0x0e */
+  uint8_t power;            /* 0x0f */
 
   /* -- GPIO block -- */
   uint8_t gpio_dir_a;       /* 0x10 */
@@ -24,11 +22,37 @@ struct i2c_registers {
   uint8_t gpio_irq_fall_b;  /* 0x17 */
   uint8_t gpio_irq_stat_a;  /* 0x18 */
   uint8_t gpio_irq_stat_b;  /* 0x19 */
-  uint8_t padding1[6];      /* 0x1a - 0x1f */
+  uint8_t gpio_pull_ena_a;  /* 0x1a */
+  uint8_t gpio_pull_ena_b;  /* 0x1b */
+  uint8_t gpio_pull_dir_a;  /* 0x1c */
+  uint8_t gpio_pull_dir_b;  /* 0x1d */
+  uint8_t padding1[2];      /* 0x1e - 0x1f */
 
   /* -- RTC block -- */
   uint8_t seconds[4];       /* 0x20 - 0x23 */
+  uint8_t alarm_seconds[4]; /* 0x24 - 0x27 */
+  uint8_t wdt_seconds;      /* 0x28 */
 };
+
+#define REG_IRQ_GPIO_MASK         (1 << 0)
+#define REG_IRQ_KEYPAD_MASK       (1 << 1)
+#define REG_IRQ_POWER_MASK        (1 << 2)
+#define REG_IRQ_ALARM_MASK        (1 << 3)
+
+#define REG_POWER_STATE_MASK      (3 << 0)
+#define REG_POWER_STATE_UP        (0 << 0)
+#define REG_POWER_STATE_DOWN      (1 << 0)
+#define REG_POWER_STATE_REBOOT    (2 << 0)
+#define REG_POWER_WDT_MASK        (1 << 2)
+#define REG_POWER_WDT_DISABLE     (0 << 2)
+#define REG_POWER_WDT_ENABLE      (1 << 2)
+#define REG_POWER_AC_STATUS_MASK  (1 << 3)
+#define REG_POWER_AC_STATUS_SHIFT (3)
+#define REG_POWER_PB_STATUS_MASK  (1 << 4)
+#define REG_POWER_PB_STATUS_SHIFT (4)
+#define REG_POWER_KEY_MASK        (3 << 6)
+#define REG_POWER_KEY_READ        (1 << 6)
+#define REG_POWER_KEY_WRITE       (2 << 6)
 
 extern struct i2c_registers registers;
 
