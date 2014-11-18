d769bda90d7aa7cdffe2d78ce4271ed54587294f
Sean Cross <xobs@kosagi.com>
2014-11-18 14:40:01 +0800

    power: Add I-class functions
    Since we're broadcasting events now, add I-class functions for powering
    on and off.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- senoko/power.c
+++ senoko/power.c
@@ -2,40 +2,76 @@
 #include "hal.h"
 
 #include "chg.h"
+#include "senoko-wdt.h"
+#include "senoko-events.h"
+
+#define REBOOT_QUIESCE_MS 300
 
 enum power_state {
   power_off = 0,
   power_on = 1,
 };
 
-#define THREAD_SLEEP_MS 1000
-//#define TESTING_POWER
+#define TESTING_POWER
 static uint32_t *power_state = ((uint32_t *)(0x40006c00 + 0x18));
 
-#include "senoko.h"
-#include "chprintf.h"
-static void power_set_state(enum power_state state) {
+static void power_set_state_x(enum power_state state) {
 #ifdef TESTING_POWER
 #warning "Testing power: Won't turn off"
   palWritePad(GPIOB, PB15, power_on);
 #else
   palWritePad(GPIOB, PB15, state);
 #endif
+
+  /* Save the value in a persistent register, in case we crash */
   *power_state = ((*power_state) & ~1) | (!state);
   return;
 }
 
+static enum power_state power_state_x(void) {
+  return (!((*power_state) & 1));
+}
+
+static void power_set_state_i(enum power_state state) {
+  if (state != power_state_x()) {
+    power_set_state_x(state);
+
+    chSysLockFromISR();
+    if (state == power_on)
+      chEvtBroadcastI(&powered_on);
+    else if (state == power_off)
+      chEvtBroadcastI(&powered_off);
+    chSysUnlockFromISR();
+  }
+}
+
+static void power_set_state(enum power_state state) {
+  if (state != power_state_x()) {
+    power_set_state_x(state);
+
+    if (state == power_on)
+      chEvtBroadcast(&powered_on);
+    else if (state == power_off)
+      chEvtBroadcast(&powered_off);
+  }
+}
+
+static virtual_timer_t power_on_vt;
+static void call_power_on(void *arg) {
+  (void)arg;
+  power_set_state_i(power_on);
+}
+
 void powerOff(void) {
   power_set_state(power_off);
+  senokoWatchdogDisable();
   return;
 }
 
-int powerIsOn(void) {
-  return (!((*power_state) & 1)) == power_on;
-}
-
-int powerIsOff(void) {
-  return (!((*power_state) & 1)) == power_off;
+void powerOffI(void) {
+  power_set_state_i(power_off);
+  senokoWatchdogDisable();
+  return;
 }
 
 void powerOn(void) {
@@ -43,11 +79,36 @@ void powerOn(void) {
   return;
 }
 
-void powerToggle(void) {
+void powerOnI(void) {
+  power_set_state_i(power_on);
+  return;
+}
+
+void powerRebootI(void) {
+  powerOffI();
+  chSysLockFromISR();
+  chVTSetI(&power_on_vt, MS2ST(REBOOT_QUIESCE_MS), call_power_on, NULL);
+  chSysUnlockFromISR();
+}
+
+void powerReboot(void) {
+  powerOff();
+  chVTSet(&power_on_vt, MS2ST(REBOOT_QUIESCE_MS), call_power_on, NULL);
+}
+
+int powerIsOn(void) {
+  return power_state_x() == power_on;
+}
+
+int powerIsOff(void) {
+  return power_state_x() == power_off;
+}
+
+void powerToggleI(void) {
   if (powerIsOn())
-    powerOff();
+    powerOffI();
   else
-    powerOn();
+    powerOnI();
   return;
 }
 
@@ -56,4 +117,5 @@ void powerInit(void) {
     powerOn();
   else
     powerOff();
+  return;
 }
--- senoko/power.h
+++ senoko/power.h
@@ -1,11 +1,13 @@
 #ifndef __SENOKO_POWER_H__
 #define __SENOKO_POWER_H__
 
-void powerOff(void);
-void powerOn(void);
+void powerOffI(void);
+void powerOnI(void);
 int powerIsOn(void);
 int powerIsOff(void);
 void powerToggle(void);
 void powerInit(void);
+void powerReboot(void);
+void powerRebootI(void);
 
 #endif /* __SENOKO_POWER_H__ */
