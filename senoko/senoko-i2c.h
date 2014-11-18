607dfbde79ac7766e45e8cc7a563a7698871d55e
Sean Cross <xobs@kosagi.com>
2014-11-18 19:55:30 +0800

    senoko-i2c: Add header definitions for i2c logging
    Since we're running without JTAG, it can be difficult to monitor
    what's going on with I2C.  Add definitions to allow for logging of
    I2C operations.
    
    Define I2C_LOGGING in order to enable.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- senoko/senoko-i2c.h
+++ senoko/senoko-i2c.h
@@ -5,6 +5,27 @@
 #include "hal.h"
 #include "i2c.h"
 
+#ifdef I2C_LOGGING
+#define I2C_LOG_ENTRIES 40
+#define I2C_LOG_DATA_SIZE 6
+struct i2clog_entry {
+  uint8_t type;
+  uint8_t size;
+  uint8_t data[I2C_LOG_DATA_SIZE];
+};
+
+struct i2clog {
+  uint32_t head;
+  struct i2clog_entry entries[I2C_LOG_ENTRIES];
+};
+
+extern struct i2clog i2clog;
+#define I2C_ENTRY_TYPE_NONE 0
+#define I2C_ENTRY_TYPE_READ 1
+#define I2C_ENTRY_TYPE_WRITE 2
+#define I2C_ENTRY_TYPE_START 3
+#endif /* I2C_LOGGING */
+
 void senokoI2cInit(void);
 msg_t senokoI2cMasterTransmitTimeout(i2caddr_t addr,
                                      const uint8_t *txbuf, size_t txbytes,
