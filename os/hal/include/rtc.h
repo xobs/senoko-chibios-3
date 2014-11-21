87384f8f931c7363e2bc2828f41b12e9e7e397e9
Sean Cross <xobs@kosagi.com>
2014-11-21 15:16:30 +0800

    os: hal: Bring RTC support from upstream
    For some reason, the git repo doesn't seem to be updating.  Manually
    pull in upstream RTC support for STM32.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- os/hal/include/rtc.h
+++ os/hal/include/rtc.h
@@ -35,6 +35,8 @@
 
 #if HAL_USE_RTC || defined(__DOXYGEN__)
 
+#include <time.h>
+
 /*===========================================================================*/
 /* Driver constants.                                                         */
 /*===========================================================================*/
@@ -91,7 +93,7 @@ typedef struct RTCDriver RTCDriver;
  * @brief   Type of a structure representing an RTC date/time stamp.
  */
 typedef struct {
-  uint32_t      year:8;             /**< @brief Years since 1980.           */
+  uint32_t      year: 8;            /**< @brief Years since 1980.           */
   uint32_t      month: 4;           /**< @brief Months 1..12.               */
   uint32_t      dstflag: 1;         /**< @brief DST correction flag.        */
   uint32_t      dayofweek: 3;       /**< @brief Day of week 1..7.           */
@@ -104,71 +106,6 @@ typedef struct {
 /*===========================================================================*/
 /* Driver macros.                                                            */
 /*===========================================================================*/
-
-/**
- * @brief   Set current time.
- *
- * @param[in] rtcp      pointer to RTC driver structure
- * @param[in] timespec  pointer to a @p RTCDateTime structure
- *
- * @iclass
- */
-#define rtcSetTimeI(rtcp, timespec)                                         \
-  rtc_lld_set_time(rtcp, timespec)
-
-/**
- * @brief   Get current time.
- *
- * @param[in] rtcp      pointer to RTC driver structure
- * @param[out] timespec pointer to a @p RTCDateTime structure
- *
- * @iclass
- */
-#define rtcGetTimeI(rtcp, timespec)                                         \
-  rtc_lld_get_time(rtcp, timespec)
-
-#if (RTC_ALARMS > 0) || defined(__DOXYGEN__)
-/**
- * @brief   Set alarm time.
- *
- * @param[in] rtcp      pointer to RTC driver structure
- * @param[in] alarm     alarm identifier
- * @param[in] alarmspec pointer to a @p RTCAlarm structure or @p NULL
- *
- * @iclass
- */
-#define rtcSetAlarmI(rtcp, alarm, alarmspec)                                \
-  rtc_lld_set_alarm(rtcp, alarm, alarmspec)
-
-/**
- * @brief   Get current alarm.
- * @note    If an alarm has not been set then the returned alarm specification
- *          is not meaningful.
- *
- * @param[in] rtcp      pointer to RTC driver structure
- * @param[in] alarm     alarm identifier
- * @param[out] alarmspec pointer to a @p RTCAlarm structure
- *
- * @iclass
- */
-#define rtcGetAlarmI(rtcp, alarm, alarmspec)                                \
-  rtc_lld_get_alarm(rtcp, alarm, alarmspec)
-#endif /* RTC_ALARMS > 0 */
-
-#if RTC_SUPPORTS_CALLBACKS || defined(__DOXYGEN__)
-/**
- * @brief   Enables or disables RTC callbacks.
- * @details This function enables or disables the callback, use a @p NULL
- *          pointer in order to disable it.
- *
- * @param[in] rtcp      pointer to RTC driver structure
- * @param[in] callback  callback function pointer or @p NULL
- *
- * @iclass
- */
-#define rtcSetCallbackI(rtcp, callback)                                     \
-  rtc_lld_set_callback(rtcp, callback)
-#endif /* RTC_SUPPORTS_CALLBACKS */
 
 /*===========================================================================*/
 /* External declarations.                                                    */
@@ -189,7 +126,12 @@ extern "C" {
 #if RTC_SUPPORTS_CALLBACKS
   void rtcSetCallback(RTCDriver *rtcp, rtccb_t callback);
 #endif
-  uint32_t rtcConvertDateTimeToFAT(RTCDateTime *timespec);
+  void rtcConvertDateTimeToStructTm(const RTCDateTime *timespec,
+                                    struct tm *timp);
+  void rtcConvertStructTmToDateTime(const struct tm *timp,
+                                    uint32_t tv_msec,
+                                    RTCDateTime *timespec);
+  uint32_t rtcConvertDateTimeToFAT(const RTCDateTime *timespec);
 #ifdef __cplusplus
 }
 #endif
--- os/hal/src/rtc.c
+++ os/hal/src/rtc.c
@@ -46,6 +46,13 @@
 /* Driver local variables and types.                                         */
 /*===========================================================================*/
 
+/*
+ * Lookup table with months' length
+ */
+static const uint8_t month_len[12] = {
+  31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
 /*===========================================================================*/
 /* Driver local functions.                                                   */
 /*===========================================================================*/
@@ -68,47 +75,61 @@ void rtcInit(void) {
 
 /**
  * @brief   Set current time.
+ * @note    This function can be called from any context but limitations
+ *          could be imposed by the low level implementation. It is
+ *          guaranteed that the function can be called from thread
+ *          context.
+ * @note    The function can be reentrant or not reentrant depending on
+ *          the low level implementation.
  *
  * @param[in] rtcp      pointer to RTC driver structure
  * @param[in] timespec  pointer to a @p RTCDateTime structure
  *
- * @api
+ * @special
  */
 void rtcSetTime(RTCDriver *rtcp, const RTCDateTime *timespec) {
 
   osalDbgCheck((rtcp != NULL) && (timespec != NULL));
 
-  osalSysLock();
-  rtcSetTimeI(rtcp, timespec);
-  osalSysUnlock();
+  rtc_lld_set_time(rtcp, timespec);
 }
 
 /**
  * @brief   Get current time.
+ * @note    This function can be called from any context but limitations
+ *          could be imposed by the low level implementation. It is
+ *          guaranteed that the function can be called from thread
+ *          context.
+ * @note    The function can be reentrant or not reentrant depending on
+ *          the low level implementation.
  *
  * @param[in] rtcp      pointer to RTC driver structure
  * @param[out] timespec pointer to a @p RTCDateTime structure
  *
- * @api
+ * @special
  */
 void rtcGetTime(RTCDriver *rtcp, RTCDateTime *timespec) {
 
   osalDbgCheck((rtcp != NULL) && (timespec != NULL));
 
-  osalSysLock();
-  rtcGetTimeI(rtcp, timespec);
-  osalSysUnlock();
+  rtc_lld_get_time(rtcp, timespec);
 }
 
 #if (RTC_ALARMS > 0) || defined(__DOXYGEN__)
 /**
  * @brief   Set alarm time.
+ * @note    This function can be called from any context but limitations
+ *          could be imposed by the low level implementation. It is
+ *          guaranteed that the function can be called from thread
+ *          context.
+ * @note    The function can be reentrant or not reentrant depending on
+ *          the low level implementation.
  *
  * @param[in] rtcp      pointer to RTC driver structure
  * @param[in] alarm     alarm identifier
  * @param[in] alarmspec pointer to a @p RTCAlarm structure or @p NULL
  *
- * @api
+ * @special
  */
 void rtcSetAlarm(RTCDriver *rtcp,
                  rtcalarm_t alarm,
@@ -116,21 +137,25 @@ void rtcSetAlarm(RTCDriver *rtcp,
 
   osalDbgCheck((rtcp != NULL) && (alarm < RTC_ALARMS));
 
-  osalSysLock();
-  rtcSetAlarmI(rtcp, alarm, alarmspec);
-  osalSysUnlock();
+  rtc_lld_set_alarm(rtcp, alarm, alarmspec);
 }
 
 /**
  * @brief   Get current alarm.
  * @note    If an alarm has not been set then the returned alarm specification
  *          is not meaningful.
+ * @note    This function can be called from any context but limitations
+ *          could be imposed by the low level implementation. It is
+ *          guaranteed that the function can be called from thread
+ *          context.
+ * @note    The function can be reentrant or not reentrant depending on
+ *          the low level implementation.
  *
  * @param[in] rtcp      pointer to RTC driver structure
  * @param[in] alarm     alarm identifier
  * @param[out] alarmspec pointer to a @p RTCAlarm structure
  *
- * @api
+ * @special
  */
 void rtcGetAlarm(RTCDriver *rtcp,
                  rtcalarm_t alarm,
@@ -138,9 +163,7 @@ void rtcGetAlarm(RTCDriver *rtcp,
 
   osalDbgCheck((rtcp != NULL) && (alarm < RTC_ALARMS) && (alarmspec != NULL));
 
-  osalSysLock();
-  rtcGetAlarmI(rtcp, alarm, alarmspec);
-  osalSysUnlock();
+  rtc_lld_get_alarm(rtcp, alarm, alarmspec);
 }
 #endif /* RTC_ALARMS > 0 */
 
@@ -149,36 +172,118 @@ void rtcGetAlarm(RTCDriver *rtcp,
  * @brief   Enables or disables RTC callbacks.
  * @details This function enables or disables the callback, use a @p NULL
  *          pointer in order to disable it.
+ * @note    This function can be called from any context but limitations
+ *          could be imposed by the low level implementation. It is
+ *          guaranteed that the function can be called from thread
+ *          context.
+ * @note    The function can be reentrant or not reentrant depending on
+ *          the low level implementation.
  *
  * @param[in] rtcp      pointer to RTC driver structure
  * @param[in] callback  callback function pointer or @p NULL
  *
- * @api
+ * @special
  */
 void rtcSetCallback(RTCDriver *rtcp, rtccb_t callback) {
 
   osalDbgCheck(rtcp != NULL);
 
-  osalSysLock();
-  rtcSetCallbackI(rtcp, callback);
-  osalSysUnlock();
+  rtc_lld_set_callback(rtcp, callback);
 }
 #endif /* RTC_SUPPORTS_CALLBACKS */
 
 /**
+ * @brief   Convert @p RTCDateTime to broken-down time structure.
+ *
+ * @param[in]  timespec   pointer to a @p RTCDateTime structure
+ * @param[out] timp       pointer to a broken-down time structure
+ *
+ * @api
+ */
+void rtcConvertDateTimeToStructTm(const RTCDateTime *timespec,
+                                  struct tm *timp) {
+  uint32_t tmp;
+
+  timp->tm_year  = timespec->year + (1980 - 1900);
+  timp->tm_mon   = timespec->month - 1;
+  timp->tm_mday  = timespec->day;
+  timp->tm_isdst = timespec->dstflag;
+
+  tmp = timespec->millisecond / 1000;
+  timp->tm_sec  = tmp % 60;
+  tmp -= timp->tm_sec;
+  timp->tm_min  = (tmp % 3600) / 60;
+  tmp -= timp->tm_min * 60;
+  timp->tm_hour = tmp / 3600;
+}
+
+/**
+ * @brief   Convert broken-down time structure to @p RTCDateTime.
+ *
+ * @param[in] timp        pointer to a broken-down time structure
+ * @param[in] tv_msec     milliseconds value
+ * @param[out] timespec   pointer to a @p RTCDateTime structure
+ *
+ * @api
+ */
+void rtcConvertStructTmToDateTime(const struct tm *timp,
+                                  uint32_t tv_msec,
+                                  RTCDateTime *timespec) {
+
+  timespec->year      = timp->tm_year - (1980 - 1900);
+  timespec->month     = timp->tm_mon + 1;
+  timespec->day       = timp->tm_mday;
+  timespec->dayofweek = timp->tm_wday + 1;
+  if (-1 == timp->tm_isdst)
+    timespec->dstflag = 0;  /* set zero if dst is unknown */
+  else
+    timespec->dstflag = timp->tm_isdst;
+  timespec->millisecond = tv_msec +
+      (timp->tm_hour * 3600 + timp->tm_min * 60 + timp->tm_sec) * 1000;
+}
+
+/**
  * @brief   Get current time in format suitable for usage in FAT file system.
+ * @note    The information about day of week and DST is lost in DOS
+ *          format, the second field loses its least significant bit.
  *
  * @param[out] timespec pointer to a @p RTCDateTime structure
  * @return              FAT date/time value.
  *
  * @api
  */
-uint32_t rtcConvertDateTimeToFAT(RTCDateTime *timespec) {
+uint32_t rtcConvertDateTimeToFAT(const RTCDateTime *timespec) {
+  uint32_t fattime;
+  uint32_t sec, min, hour, day, month, tmp;
 
-  osalDbgCheck(timespec != NULL);
+  tmp = timespec->millisecond / 1000;
+  sec = tmp % 60;
+  min = (tmp - sec) % 3600;
+  hour = (tmp - sec - min * 60) / 3600;
+  day = timespec->day;
+  month = timespec->month;
 
-  /* TODO: Implement.*/
-  return 0;
+  /* handle DST flag */
+  if (1 == timespec->dstflag) {
+    hour += 1;
+    if (hour == 24) {
+      hour = 0;
+      day += 1;
+      if (day > month_len[month - 1]) {
+        day = 1;
+        month += 1;
+      }
+    }
+  }
+
+  fattime  = sec            >> 1;
+  fattime |= min            << 5;
+  fattime |= hour           << 11;
+  fattime |= day            << 16;
+  fattime |= month          << 21;
+  fattime |= timespec->year << 25;
+
+  return fattime;
 }
 
 #endif /* HAL_USE_RTC */
